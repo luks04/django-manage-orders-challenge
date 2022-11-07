688535a1d16308c67c6b9d7146c7cdaa6d0e5097
luks04 <67431911+luks04@users.noreply.github.com>
2022-11-07 18:57:41 -0500

    Set Debug to False in main branch

--- app/orders_challenge/settings.py
+++ app/orders_challenge/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h9w(&ai03x5@%#93kas*lckweg&1gt-*xdf@u%w+1hky+=3q70'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
